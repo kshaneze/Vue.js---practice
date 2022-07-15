698bbd2f0d952a08e36a5a9acb44acab9af71b21
niksa <niksaopfor@gmail.com>
2022-07-15 22:11:43 +0200

    first commit

---
+++ src/components/FriendContact.vue
@@ -0,0 +1,58 @@
+<template>
+  <li>
+    <h2>{{ name }} {{ isFavorite ? '(Vissible)' : '' }}</h2>
+    <button @click="toggleFavorite">Toggle Favorite</button>
+    <button @click="toggleDetails">
+      {{ detailsAreVisible ? 'Hide Details' : 'Show Details' }}
+    </button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone:</strong>{{ phoneNumber }}</li>
+      <li><strong>Email:</strong>{{ emailAddress }}</li>
+    </ul>
+    <button @click="$emit('delete-friend', id)">Delete</button>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    id: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    emailAddress: {
+      type: String,
+      required: true,
+    },
+
+    isFavorite: {
+      type: String,
+      required: true,
+    },
+  },
+  emits: ['toggle-favorite', 'delete-friend'],
+  data() {
+    return {
+      detailsAreVisible: false,
+    }
+  },
+
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible
+    },
+
+    toggleFavorite() {
+      this.$emit('toggle-favorite', this.id)
+    },
+  },
+}
+</script>
---
+++ src/components/NewFriend.vue
@@ -0,0 +1,31 @@
+<template>
+  <form>
+    <input type="text" placeholder="Name" v-model="friendName" />
+    <input type="tel" placeholder="Phone" v-model="friendPhone" />
+    <input type="email" placeholder="Email" v-model="friendEmail" />
+    <button @click.prevent="submitFriendsInformation">Create Friend</button>
+  </form>
+</template>
+<script>
+export default {
+  emits: ['add-contact'],
+  data() {
+    return {
+      friendName: '',
+      friendPhone: '',
+      friendEmail: '',
+    }
+  },
+
+  methods: {
+    submitFriendsInformation() {
+      this.$emit(
+        'add-contact',
+        this.friendName,
+        this.friendPhone,
+        this.friendEmail
+      )
+    },
+  },
+}
+</script>
